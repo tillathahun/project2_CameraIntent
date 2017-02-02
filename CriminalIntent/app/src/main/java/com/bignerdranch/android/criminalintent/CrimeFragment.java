cede1cce30964b566631fc96ff29f08099f42738
Tyler Bennett <tylerbennett215@gmail.com>
2017-02-02 10:41:20 -0500

    commit, really for nothing but im gonna do it anyway

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -6,16 +6,23 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.util.SparseArrayCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,6 +33,13 @@ import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+import org.w3c.dom.Text;
 
 import java.io.File;
 import java.util.Date;
@@ -57,6 +71,9 @@ public class CrimeFragment extends Fragment {
     private ImageView mPhotoView4;
     private CheckBox mFaceDetect;
     private boolean mCanTakePhoto;
+    private TextView mFaceCount;
+    private Paint mPaint;
+    private SparseArray<Face> mFaces;
 
     public static CrimeFragment newInstance(UUID crimeId) {
         Bundle args = new Bundle();
@@ -76,6 +93,7 @@ public class CrimeFragment extends Fragment {
         mPhotoFile2 = CrimeLab.get(getActivity()).getPhotoFile(mCrime, mCrime.getPhotoCount()-1);
         mPhotoFile3 = CrimeLab.get(getActivity()).getPhotoFile(mCrime, mCrime.getPhotoCount()-2);
         mPhotoFile4 = CrimeLab.get(getActivity()).getPhotoFile(mCrime, mCrime.getPhotoCount()-3);
+        mPaint = new Paint();
     }
 
     @Override
@@ -138,6 +156,7 @@ public class CrimeFragment extends Fragment {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 mCrime.setFaceDetect(isChecked);
+                updatePhotoView();
             }
         });
 
@@ -173,6 +192,8 @@ public class CrimeFragment extends Fragment {
                 PackageManager.MATCH_DEFAULT_ONLY) == null) {
             mSuspectButton.setEnabled(false);
         }
+
+        mFaceCount = (TextView) v.findViewById(R.id.faceCount);
 
         mPhotoButton = (ImageButton) v.findViewById(R.id.crime_camera);
         final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -286,7 +307,40 @@ public class CrimeFragment extends Fragment {
         } else {
             Bitmap bitmap = PictureUtils.getScaledBitmap(
                     mPhotoFile.getPath(), getActivity());
-            mPhotoView.setImageBitmap(bitmap);
+
+            Matrix matrix = new Matrix();
+            matrix.postRotate(-90);
+            Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap , 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+
+            if(mFaceDetect.isChecked()){
+                FaceDetector detector = new FaceDetector.Builder( getContext() )
+                        .setTrackingEnabled(false)
+                        .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                        .setMode(FaceDetector.FAST_MODE)
+                        .build();
+                if (!detector.isOperational()) {
+                    //Handle contingency
+                } else {
+                    Frame frame = new Frame.Builder().setBitmap(rotatedBitmap).build();
+                    mFaces = detector.detect(frame);
+                    detector.release();
+                    setNumFaceText(mFaces.size());
+                }
+                //invalidate(); //NOT SURE???
+
+                Bitmap tempBitmap = Bitmap.createBitmap(rotatedBitmap.getWidth(), rotatedBitmap.getHeight(), Bitmap.Config.RGB_565);
+                Canvas tempCanvas = new Canvas(tempBitmap);
+
+                tempCanvas.drawBitmap(rotatedBitmap, 0, 0, null);
+                drawFaceBox(tempCanvas, 1);
+
+
+                mPhotoView.setImageDrawable(new BitmapDrawable(getResources(), tempBitmap));
+            }
+            else {
+                mPhotoView.setImageBitmap(rotatedBitmap);
+                setNumFaceText(-1);
+            }
         }
 
         if (mPhotoFile2 == null || !mPhotoFile2.exists()) {
@@ -311,6 +365,43 @@ public class CrimeFragment extends Fragment {
             Bitmap bitmap = PictureUtils.getScaledBitmap(
                     mPhotoFile4.getPath(), getActivity());
             mPhotoView4.setImageBitmap(bitmap);
+        }
+    }
+
+    private void drawFaceBox(Canvas canvas, double scale) {
+        //paint should be defined as a member variable rather than
+        //being created on each onDraw request, but left here for
+        //emphasis.
+
+        mPaint.setColor(Color.GREEN);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(5);
+
+        float left = 0;
+        float top = 0;
+        float right = 0;
+        float bottom = 0;
+
+        for( int i = 0; i < mFaces.size(); i++ ) {
+            Face face = mFaces.valueAt(i);
+
+            left = (float) ( face.getPosition().x * scale );
+            top = (float) ( face.getPosition().y * scale );
+            right = (float) scale * ( face.getPosition().x + face.getWidth() );
+            bottom = (float) scale * ( face.getPosition().y + face.getHeight() );
+
+            canvas.drawRect( left, top, right, bottom, mPaint );
+        }
+    }
+    private void  setNumFaceText(int faceCount){
+        if (faceCount == 0) {
+            mFaceCount.setText("No Faces Detected");
+        } else if (faceCount == 1) {
+            mFaceCount.setText("1 face found");
+        } else if (faceCount == -1) {
+            mFaceCount.setText("Face detection off");
+        } else {
+            mFaceCount.setText(faceCount + " faces found");
         }
     }
 }
